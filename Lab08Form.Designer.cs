6fd3b213aa558b79e6ac96b4b4b0d96fe538d351
PrinceSharma001 <52267273+PrinceSharma001@users.noreply.github.com>
2019-06-26 23:30:05 -0400

    Add files via upload

---
+++ Lab08Form.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace COMP123_S2019_Lesson8_Lab08
+{
+    partial class Lab8Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.InfoGroupBox = new System.Windows.Forms.GroupBox();
+            this.AgeTextbox = new System.Windows.Forms.TextBox();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.NameTextbox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.InfoGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.TitleLabel.Location = new System.Drawing.Point(17, 16);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(112, 39);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Lab08";
+            // 
+            // InfoGroupBox
+            // 
+            this.InfoGroupBox.BackColor = System.Drawing.Color.LightGray;
+            this.InfoGroupBox.Controls.Add(this.AgeTextbox);
+            this.InfoGroupBox.Controls.Add(this.AgeLabel);
+            this.InfoGroupBox.Controls.Add(this.NameTextbox);
+            this.InfoGroupBox.Controls.Add(this.NameLabel);
+            this.InfoGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.InfoGroupBox.Location = new System.Drawing.Point(25, 73);
+            this.InfoGroupBox.Margin = new System.Windows.Forms.Padding(4);
+            this.InfoGroupBox.Name = "InfoGroupBox";
+            this.InfoGroupBox.Padding = new System.Windows.Forms.Padding(4);
+            this.InfoGroupBox.Size = new System.Drawing.Size(791, 246);
+            this.InfoGroupBox.TabIndex = 1;
+            this.InfoGroupBox.TabStop = false;
+            this.InfoGroupBox.Text = "Info";
+            // 
+            // AgeTextbox
+            // 
+            this.AgeTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.AgeTextbox.Location = new System.Drawing.Point(253, 91);
+            this.AgeTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.AgeTextbox.Name = "AgeTextbox";
+            this.AgeTextbox.Size = new System.Drawing.Size(500, 45);
+            this.AgeTextbox.TabIndex = 2;
+            this.AgeTextbox.TextChanged += new System.EventHandler(this.AgeTextbox_TextChanged);
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.AgeLabel.Location = new System.Drawing.Point(43, 95);
+            this.AgeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(187, 39);
+            this.AgeLabel.TabIndex = 2;
+            this.AgeLabel.Text = "Enter Age:";
+            // 
+            // NameTextbox
+            // 
+            this.NameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.NameTextbox.Location = new System.Drawing.Point(253, 25);
+            this.NameTextbox.Margin = new System.Windows.Forms.Padding(4);
+            this.NameTextbox.Name = "NameTextbox";
+            this.NameTextbox.Size = new System.Drawing.Size(500, 45);
+            this.NameTextbox.TabIndex = 1;
+            this.NameTextbox.TextChanged += new System.EventHandler(this.NameTextbox_TextChanged);
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.NameLabel.Location = new System.Drawing.Point(9, 28);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(218, 39);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Enter Name:";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.BackColor = System.Drawing.Color.Aqua;
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(595, 475);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(183, 53);
+            this.SubmitButton.TabIndex = 3;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = false;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.OutputLabel.Location = new System.Drawing.Point(25, 482);
+            this.OutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(562, 38);
+            this.OutputLabel.TabIndex = 4;
+            // 
+            // Lab8Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(832, 543);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.InfoGroupBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Lab8Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab08";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Lab8Form_Load);
+            this.InfoGroupBox.ResumeLayout(false);
+            this.InfoGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        protected System.Windows.Forms.GroupBox InfoGroupBox;
+        private System.Windows.Forms.TextBox AgeTextbox;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox NameTextbox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ Lab08Form.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson8_Lab08
+{
+    public partial class Lab8Form : Form
+    {
+        public string UserName { get; set; }
+        public float UserAge { get; set; }
+        public Lab8Form()
+        {
+            InitializeComponent();
+
+        }
+         /// <summary>
+         /// Event Handler for Lab08Form Load Event
+         /// </summary>
+         /// <param name="sender"></param>
+         /// <param name="e"></param>
+        private void Lab8Form_Load(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        /// <summary>
+        /// This is the Event Handler for SubmitButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            UserName = NameTextbox.Text;
+            UserAge = float.Parse(AgeTextbox.Text);
+            OutputLabel.Text = NameTextbox.Text + " " + AgeTextbox.Text;
+            ClearForm();
+
+        }
+
+        private void ClearForm()
+        {
+            NameTextbox.Clear();
+            AgeTextbox.Clear();
+            SubmitButton.Enabled = false;
+        }
+
+        private void AgeTextbox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float.Parse(AgeTextbox.Text);
+                SubmitButton.Enabled = true;
+            }
+            catch 
+            {
+                SubmitButton.Enabled = false;
+            }
+        }
+
+        private void NameTextbox_TextChanged(object sender, EventArgs e)
+        {
+            SubmitButton.Enabled = (NameTextbox.Text.Length > 2) ? true : false;
+        }
+    }
+}
